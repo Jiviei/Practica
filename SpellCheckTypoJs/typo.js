e12cfe7449a53bc169bb437b70467fb52f4c7ab8
Jiviei <154891763+Jiviei@users.noreply.github.com>
2024-01-15 17:30:21 +0300

    Переделать выходные данные

--- SpellCheckTypoJs/typo.js
+++ SpellCheckTypoJs/typo.js
@@ -25,7 +25,36 @@ const checkSpelling_typo = (md) => {
     }
     return spellingMistakes;
 };
+const getPartsOfContent = (md, spellingMistakes) => {
+    var partOfContent = [];
+    let lines = md.split("\n");
+    let start = 0;
+    let line = 0;
+    for (var spellingMistake of spellingMistakes) {
+        if (line != spellingMistake.range.line) {
+            if (spellingMistake.range.startIdx == 0) {
+                continue;
+            }
+            else {
+                partOfContent.push({
+                    content: lines[line].substring(start) + "\n" + lines[++line].substring(0, spellingMistake.range.startIdx)
+                });
+            }
+        }
+        else
+            partOfContent.push({
+                content: lines[line].substring(start, spellingMistake.range.startIdx)
+            });
+        start = spellingMistake.range.endIdx + 1;
+        partOfContent.push({
+            content: spellingMistake.original,
+            advices: spellingMistake.advices
+        });
+    }
+    return partOfContent;
+};
 const md = `Очень, провильное преложение.\nВторая стрчка!`;
 const spellingMistakes = checkSpelling_typo(md);
-console.log(spellingMistakes);
+const partsOfContent = getPartsOfContent(md, spellingMistakes);
+console.log(partsOfContent);
 //# sourceMappingURL=typo.js.map
--- SpellCheckTypoJs/typo.ts
+++ SpellCheckTypoJs/typo.ts
@@ -8,6 +8,7 @@ interface SpellingMistake {
     original: string;
     advices: string[];
 }
+
 const checkSpelling_typo = (md: string): SpellingMistake[] => {
     var Typo = require("typo-js");
     var dictionary = new Typo('ru_RU');
@@ -36,7 +37,45 @@ const checkSpelling_typo = (md: string): SpellingMistake[] => {
     return spellingMistakes;
 }
 
+interface PartOfContent {
+    content: string
+    advices?: string[];
+}
+
+const getPartsOfContent = (
+    md: string,
+    spellingMistakes: SpellingMistake[]
+): PartOfContent[] => {
+    var partOfContent: PartOfContent[] = [];
+    let lines: string[] = md.split("\n");
+    let start = 0;
+    let line = 0;
+    for (var spellingMistake of spellingMistakes) {
+        if (line != spellingMistake.range.line) {
+            if (spellingMistake.range.startIdx == 0) {
+                continue;
+            } else {
+                partOfContent.push({
+                    content: lines[line].substring(start) + "\n" + lines[++line].substring(0, spellingMistake.range.startIdx)
+                });
+            }
+        } else
+            partOfContent.push({
+                content: lines[line].substring(start, spellingMistake.range.startIdx)
+            });
+        start = spellingMistake.range.endIdx + 1;
+        partOfContent.push({
+            content: spellingMistake.original,
+            advices: spellingMistake.advices
+        });
+    }
+    return partOfContent;
+}
+
 const md = `Очень, провильное преложение.\nВторая стрчка!`;
 
 const spellingMistakes: SpellingMistake[] = checkSpelling_typo(md);
-console.log(spellingMistakes)
+
+const partsOfContent = getPartsOfContent(md, spellingMistakes);
+
+console.log(partsOfContent)
